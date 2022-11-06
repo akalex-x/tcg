e798250ee121fc8586a72da22e02b59259f68193
Alex K <alex.kinejara@gmail.com>
2022-11-06 10:42:26 +0800

    added dvd loading screen

---
+++ src/components/dvd/index.js
@@ -0,0 +1,89 @@
+import styles from './dvd.module.scss'
+import { useEffect } from 'react'
+
+function DVD(){
+
+    useEffect( () => {
+
+        let speed = 20;
+        let scale = 0.3; // Image scale (I work on 1080p monitor)
+        let canvas;
+        let ctx;
+        let logoColor;
+
+        let dvd = {
+            x: 200,
+            y: 300,
+            xspeed: 10,
+            yspeed: 10,
+            img: new Image()
+        };
+
+        (function main(){
+            canvas = document.getElementById("tv-screen");
+            ctx = canvas.getContext("2d");
+            dvd.img.src = '/dvd-logo.png';
+
+            //Draw the "tv screen"
+            canvas.width  = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            pickColor();
+            update();
+        })();
+
+        function update() {
+            setTimeout(() => {
+                //Draw the canvas background
+                ctx.fillStyle = '#10069f';
+                ctx.fillRect(0, 0, canvas.width, canvas.height);
+                //Draw DVD Logo and his background
+                ctx.fillStyle = logoColor;
+                ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+                ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+                //Move the logo
+                dvd.x+=dvd.xspeed;
+                dvd.y+=dvd.yspeed;
+                //Check for collision 
+                checkHitBox();
+                update();   
+            }, speed)
+        }
+
+        //Check for border collision
+        function checkHitBox(){
+            if(dvd.x+dvd.img.width*scale >= canvas.width || dvd.x <= 0){
+                dvd.xspeed *= -1;
+                pickColor();
+            }
+                
+            if(dvd.y+dvd.img.height*scale >= canvas.height || dvd.y <= 0){
+                dvd.yspeed *= -1;
+                pickColor();
+            }    
+        }
+
+        //Pick a random color in RGB format
+        function pickColor(){
+            // let r = Math.random() * (254 - 0) + 0;
+            let r = '255';
+            // let g = Math.random() * (254 - 0) + 0;
+            let g = '255';
+            // let b = Math.random() * (254 - 0) + 0;
+            let b = '255';
+
+            // logoColor = 'rgb('+r+','+g+', '+b+')';
+            logoColor = '#10069f';
+        }
+
+    },[]);
+
+    return(
+        <>
+            <canvas id="tv-screen" className={styles.dvd}></canvas>
+        </>
+    )
+
+}
+
+export default DVD
--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,36 @@
 import Header from 'components/header'
 import Footer from 'components/footer'
+import DVD from 'components/dvd'
+import { useEffect, useState } from 'react'
 
 function Layout({ children }) {
+
+    const [showDVD,setDVD] = useState(false)
+
+    let showCounter = setTimeout(function(){
+        !showDVD ? setDVD(true) : null 
+    },30000)
+
+    const checkDVD = () => {
+        console.log(showDVD)
+        if( showDVD ){
+            setDVD(false)
+        }
+        clearTimeout(showCounter)
+        showCounter = setTimeout(function(){
+            !showDVD ? setDVD(true) : null 
+        },30000)
+    }
+
+    useEffect(()=>{
+
+        document.addEventListener('mousemove', checkDVD)
+        document.addEventListener('scroll', checkDVD)
+
+        return () => [document.removeEventListener('mousemove', checkDVD),document.removeEventListener('scroll', checkDVD)];
+
+    },[showDVD])
+
     return(
         <>
             <Header />
@@ -9,6 +38,9 @@ function Layout({ children }) {
                 {children}
             </main>
             <Footer />
+            { showDVD &&
+                <DVD />
+            }
         </>
     )
 }
