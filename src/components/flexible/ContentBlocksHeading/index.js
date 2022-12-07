08913ca9e3609bbfaec50a2e1b7499136f08833d
Alex K <alex.kinejara@gmail.com>
2022-12-07 09:37:54 +0800

    added key to content blocks

--- src/components/flexible/ContentBlocksHeading/index.js
+++ src/components/flexible/ContentBlocksHeading/index.js
@@ -9,9 +9,9 @@ function ContentBlocksHeading({data}){
                 <div className="container">
                     <h2 className={styles.contentBlocks__heading}>{data.heading}</h2>
                     <div className={styles.contentBlocks__loop}>
-                        { data.blocks.map( (item) => {
+                        { data.blocks.map( (item,i) => {
                             return(
-                                <div className={styles.contentBlocks__block}>
+                                <div key={i} className={styles.contentBlocks__block}>
                                     <h3 className='heading'>{item.heading}</h3>
                                     <div className='content' dangerouslySetInnerHTML={{__html: item.content}}>
                                     </div>
