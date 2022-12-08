1c58b7c658b725a176899f0fedb0dce1423899f5
Alex K <alex.kinejara@gmail.com>
2022-12-09 06:52:27 +0800

    added bility to add br/ to text in content blocks

--- src/components/flexible/ContentBlocksHeading/index.js
+++ src/components/flexible/ContentBlocksHeading/index.js
@@ -7,12 +7,12 @@ function ContentBlocksHeading({data}){
             {/* {console.log(data)} */}
             <section className={styles.contentBlocks}>
                 <div className="container">
-                    <h2 className={styles.contentBlocks__heading}>{data.heading}</h2>
+                    <h2 className={styles.contentBlocks__heading} dangerouslySetInnerHTML={{__html: data.heading}}></h2>
                     <div className={styles.contentBlocks__loop}>
                         { data.blocks.map( (item,i) => {
                             return(
                                 <div key={i} className={styles.contentBlocks__block}>
-                                    <h3 className='heading'>{item.heading}</h3>
+                                    <h3 className='heading' dangerouslySetInnerHTML={{__html: item.heading}}></h3>
                                     <div className='content' dangerouslySetInnerHTML={{__html: item.content}}>
                                     </div>
                                 </div>
