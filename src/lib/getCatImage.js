11f0a42b297a43e1843a53bc842b50a59269cba4
Alex K <alex.kinejara@gmail.com>
2023-01-07 08:57:00 +0800

    increased quality of cat icon

--- src/lib/getCatImage.js
+++ src/lib/getCatImage.js
@@ -4,27 +4,27 @@ export function getCatImage(cat){
 
     switch (cat) {
       case 'digital-identity':
-        return <Image alt="Digital Identity Icon" width={16} height={16} src='/cats/digital-identity.png' />
+        return <Image alt="Digital Identity Icon" width={40} height={40} src='/cats/digital-identity.png' />
 
       case 'art':
-        return <Image alt="Digital Art Icon" width={16} height={16} src='/cats/art.png' />
+        return <Image alt="Digital Art Icon" width={40} height={40} src='/cats/art.png' />
       case 'design':
-        return <Image alt="Digital Design Icon" width={16} height={16} src='/cats/design.png' />
+        return <Image alt="Digital Design Icon" width={40} height={40} src='/cats/design.png' />
       case 'education':
-        return <Image alt="Digital Educatin Icon" width={16} height={16} src='/cats/education.png' />
+        return <Image alt="Digital Educatin Icon" width={40} height={40} src='/cats/education.png' />
       case 'mobile':
-        return <Image alt="Digital Mobile Icon" width={16} height={16} src='/cats/mobile.png' />
+        return <Image alt="Digital Mobile Icon" width={40} height={40} src='/cats/mobile.png' />
       case 'music':
-        return <Image alt="Digital Music Icon" width={16} height={16} src='/cats/music.png' />
+        return <Image alt="Digital Music Icon" width={40} height={40} src='/cats/music.png' />
 
       case 'utility':
-        return <Image alt="Utility Icon" width={16} height={16} src='/cats/utility.png' />
+        return <Image alt="Utility Icon" width={40} height={40} src='/cats/utility.png' />
       case 'gaming':
-        return <Image alt="Gaming Icon" width={16} height={16} src='/cats/gaming.png' />
+        return <Image alt="Gaming Icon" width={40} height={40} src='/cats/gaming.png' />
       case 'entertainment':
-        return <Image alt="Entertainment Icon" width={16} height={16} src='/cats/entertainment.png' />
+        return <Image alt="Entertainment Icon" width={40} height={40} src='/cats/entertainment.png' />
       case 'collectives':
-        return <Image alt="Collectives Icon" width={16} height={16} src='/cats/collectives.png' />
+        return <Image alt="Collectives Icon" width={40} height={40} src='/cats/collectives.png' />
     }
 
 }
